6167cbcef96ab095d3b888ff0b5bda90debcaf6e
Sergey Sukhoverkhov <seregas@gmail.com>
2021-08-22 11:57:40 +0300

    Создание класса

---
+++ src/main/java/com/sbrf/reboot/service/Account.java
@@ -0,0 +1,9 @@
+package com.sbrf.reboot.service;
+
+public class Account {
+    private String accountNumber;
+
+    public Account(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+}

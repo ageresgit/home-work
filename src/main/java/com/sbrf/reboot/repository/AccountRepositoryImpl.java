27b365e66d589a5efda10a49b2c7b35de30a68ef
Sergey Sukhoverkhov <seregas@gmail.com>
2021-08-23 14:27:43 +0300

    Восстановил из локальной истории файла работу с JSON как с текстом. Добавлено 3 неиспользуемых private метода.

--- src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
+++ src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbrf.reboot.account.Account;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class AccountRepositoryImpl implements AccountRepository {
@@ -34,6 +36,68 @@ public class AccountRepositoryImpl implements AccountRepository {
         for (AccountParser ap : apList) {
             if (ap.getClientId() > 0 && ap.getNumber() != null && !ap.getNumber().isEmpty()) {
                 addClientAccount(ap.getClientId(), new Account(ap.getNumber()));
+            }
+        }
+    }
+
+    // Методы для примитивного парсинга json как текста getClientId, getAccountNumber, importDataFromFile
+    private static long getClientId(String line) {
+        long clientId = -1;
+        int p1 = line.indexOf(": ");
+        if (p1 > 0){
+            int p2 = line.indexOf(",", p1 + 2);
+            if (p2 > 0) {
+                clientId = Long.parseLong(line.substring(p1 + 2, p2));
+            }
+        }
+
+        return clientId;
+    }
+
+    private static String getAccountNumber(String line) {
+        String accountNumber = "";
+        int p1 = line.indexOf(": ");
+        if (p1 > 0) {
+            if (line.length() - 1 - (p1 + 3) > 0) {
+                accountNumber = line.substring(p1 + 3, line.length() - 1);
+            }
+        }
+
+        return accountNumber;
+    }
+
+    private void importDataFromFile() throws IOException {
+        clientToAccounts =  new HashMap<Long, HashSet<Account>>();
+        boolean foundClient = false;
+        long clientId = -1;
+        for (String nextLine: Files.readAllLines(Paths.get(this.filePath))) {
+            if (foundClient) {
+                foundClient = false;
+                if (nextLine.contains("number") && clientId > 0) {
+                    String accountNumber =getAccountNumber(nextLine);
+                    if (!accountNumber.isEmpty()) {
+                        Account account = new Account(accountNumber);
+                        if (clientToAccounts.containsKey(clientId)) {
+                            HashSet<Account> accountsSet = clientToAccounts.get(clientId);
+
+                            if (!accountsSet.contains(account)) {
+                                accountsSet.add(account);
+                            }
+                        }
+                        else {
+                            HashSet<Account> accountsSet = new HashSet<Account>();
+                            accountsSet.add(account);
+                            clientToAccounts.put(clientId, accountsSet);
+                        }
+                    }
+                }
+
+            }
+            else {
+                if (nextLine.contains("clientId")) {
+                    foundClient = true;
+                    clientId = getClientId(nextLine);
+                }
             }
         }
     }
